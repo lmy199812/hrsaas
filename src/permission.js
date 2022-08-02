5ff3f96004ebec174f4dd008d1b91f7cbb1d87a3
偏执 <1924618143@qq.com>
2022-08-02 12:17:03 +0800

    完成主页路由权限

---
+++ src/permission.js
@@ -0,0 +1,35 @@
+import router from '@/router'
+import store from '@/store'
+// 全局路由前置守卫
+// 进入路由前调用
+// to : 去哪的路由
+// from : 来自于哪个路由
+// next : 是否进入
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  //  进行权限控制
+  //  调用了next进入该路由，不调用无法进入
+  const token = store.state.user.token
+  if (token) {
+    //  1 登录
+    //  是否进入登录页
+    if (to.path === '/login') {
+      //  1.1 是 跳到首页
+      next('/')
+    } else {
+      //  1.2 不是，直接进入
+      next()
+    }
+  } else {
+    //  2未登录
+    //  访问是否在白名单内
+    const isCludes = whiteList.includes(to.path)
+    if (isCludes) {
+      //  2.1 在白名单 方行
+      next()
+    } else {
+      //  2.2 不在白名单(不等录不能访问) 跳到登录页
+      next('/login')
+    }
+  }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -7,25 +7,6 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
-/**
  * constantRoutes
  * a base page that does not have permission requirements
  * all roles can be accessed
@@ -48,12 +29,14 @@ export const constantRoutes = [
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
-    }]
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
   },
 
   {
@@ -121,13 +104,15 @@ export const constantRoutes = [
             children: [
               {
                 path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-1'),
                 name: 'Menu1-2-1',
                 meta: { title: 'Menu1-2-1' }
               },
               {
                 path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-2'),
                 name: 'Menu1-2-2',
                 meta: { title: 'Menu1-2-2' }
               }
@@ -165,12 +150,13 @@ export const constantRoutes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
-
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
+// vueRouter 实例
 const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
