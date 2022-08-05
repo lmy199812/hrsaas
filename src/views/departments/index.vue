9816bdebb4510e4aeeb51bbdf522ace3d499b081
偏执 <1924618143@qq.com>
2022-08-05 11:32:13 +0800

    将树形内容单独抽提组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <treeTools :isRoot="true" :treeNode="company" />
+        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+        <!-- 树形 -->
+        <el-tree :data="treeData" :props="defaultProps" default-expand-all="">
+          <template v-slot="{ data }">
+            <treeTools :treeNode="data" />
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import treeTools from './components/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name'
+      },
+      company: { name: '传智播客教育', manager: '负责人' }
+    }
+  },
+  components: {
+    treeTools
   },
 
   created() {},
